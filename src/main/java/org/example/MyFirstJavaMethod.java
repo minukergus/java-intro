1c8ca4fc050e7d01d72e787f0e7fc8f97581c763
kasutaja <triintam@hotmail.com>
2023-04-02 12:42:23 +0300

    functions example final

---
+++ src/main/java/org/example/MyFirstJavaMethod.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class MyFirstJavaMethod {
+    public static
+    // function has got a name: main
+    // every function needs some input parameters - located inside braces e.g. String [] args
+    // return type (result) - void - at the beginning of the method signature (public static void (String[] args))
+
+    void main(String[] args) {
+        // call method using its name
+        describeMe();
+        System.out.println("one");
+        describeMe();
+        System.out.println("two");
+        describeMe();
+
+
+    }
+    public static  void describeMe() {
+        System.out.println("Hi, my name is Triin");
+        System.out.println("I am a future Java programmer");
+
+    }
+}
